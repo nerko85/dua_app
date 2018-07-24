dbc4b97a0eddaa7ed56beb85fe1d0f1277cc6f96
nerko85 <nerko_85@live.com>
2018-07-24 16:35:27 +0200

    test

--- .next/static/commons/main.js
+++ .next/static/commons/main.js
@@ -26,40 +26,7 @@ webpackJsonp([0],{
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1532437591268");
-          });
-      }
-    }
-  
-
-/***/ }),
-
-/***/ "./components/Layout.css":
-/***/ (function(module, exports, __webpack_require__) {
-
-// removed by extract-text-webpack-plugin;
-    if (true) {
-      var injectCss = function injectCss(prev, href) {
-        var link = prev.cloneNode();
-        link.href = href;
-        link.onload = function() {
-          prev.parentNode.removeChild(prev);
-        };
-        prev.stale = true;
-        prev.parentNode.insertBefore(link, prev);
-      };
-      module.hot.dispose(function() {
-        window.__webpack_reload_css__ = true;
-      });
-      if (window.__webpack_reload_css__) {
-        module.hot.__webpack_reload_css__ = false;
-        console.log("[HMR] Reloading stylesheets...");
-        var prefix = document.location.protocol + '//' + document.location.host;
-        document
-          .querySelectorAll("link[href][rel=stylesheet]")
-          .forEach(function(link) {
-            if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1532438255290");
+            injectCss(link, link.href.split("?")[0] + "?unix=1532441789235");
           });
       }
     }
@@ -92,7 +59,7 @@ webpackJsonp([0],{
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1532437591276");
+            injectCss(link, link.href.split("?")[0] + "?unix=1532441789243");
           });
       }
     }
@@ -125,7 +92,7 @@ webpackJsonp([0],{
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1532438228092");
+            injectCss(link, link.href.split("?")[0] + "?unix=1532441789674");
           });
       }
     }
@@ -158,7 +125,7 @@ webpackJsonp([0],{
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1532438194068");
+            injectCss(link, link.href.split("?")[0] + "?unix=1532441789239");
           });
       }
     }
